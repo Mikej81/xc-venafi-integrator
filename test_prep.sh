f14a46ad33c28bada033f628344c0c7833718e2a
Mikej81 <stumpj@gmail.com>
2024-02-27 17:18:55 -0500

    test prep

---
+++ test_prep.sh
@@ -0,0 +1,16 @@
+export VEN_API_KEY=""
+export VEN_API_URL="https://api.venafi.cloud"
+
+export VEN_APP_ID=""
+export VEN_CERT_TEMPLATE=""
+export VEN_COMMON_NAME=""
+export VEN_ORGANIZATION=""
+export VEN_ORGANIZATIONAL_UNITS=""
+export VEN_LOCALITY=""
+export VEN_STATE=""
+export VEN_COUNTRY=""
+export VEN_DNS_NAMES=""
+
+export XC_API_URL=
+export XC_API_KEY=
+export VES_P12_PASSWORD=
